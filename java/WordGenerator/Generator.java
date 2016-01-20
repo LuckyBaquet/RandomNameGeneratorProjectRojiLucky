aaa950b0c0616b6088b764f587a0a16ec102be8a
Lucky <lucky@Zipcoder03s-MacBook-Pro.local>
2016-01-20 15:08:45 -0500

    Pushing word generator to Github

---
+++ java/WordGenerator/Generator.java
@@ -0,0 +1,14 @@
+package WordGenerator;
+
+/**
+ * Created by lucky on 1/19/16.
+ */
+public class Generator {
+
+
+    public static void main(String[] args) {
+        char[] q = {'a','v','b'};
+        ArrayOfWords a = new ArrayOfWords(q,20,true);
+    }
+
+}
